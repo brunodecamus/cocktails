24ec7ece5e9238d2ab26ed923fc81d30a7ecc4c3
bruno00057 <bruno.decamus@gmail.com>
2018-10-17 22:34:46 +0200

    Ajout de trois component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CocktailsListComponent,
+    CocktailDetailsComponent
   ],
   imports: [
     BrowserModule
