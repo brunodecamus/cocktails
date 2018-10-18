35017f1ddb80d8b0e3396e505f08f2a6f32ad950
bruno00057 <bruno.decamus@gmail.com>
2018-10-18 22:37:03 +0200

    Ajout du detail du coptail

--- src/app/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-details/cocktail-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cocktail } from '../shared/cocktail.model';
 
 @Component({
   selector: 'app-cocktail-details',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cocktail-details.component.css']
 })
 export class CocktailDetailsComponent implements OnInit {
+
+  public cocktail = new Cocktail('Mojito', 'http://anotherwhiskyformisterbukowski.com/wp-content/uploads/2016/09/mojito-1.jpg', 'Le mojito, prononcé en espagnol, est un cocktail à base de rhum, de citron vert et de feuilles de menthe fraîche, né à Cuba dans les années 1910.');
 
   constructor() { }
 
