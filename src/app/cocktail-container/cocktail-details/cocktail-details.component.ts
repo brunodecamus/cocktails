cce813b9cab98d2773ac0c89eb627f8c459e141b
bruno00057 <bruno.decamus@gmail.com>
2018-10-20 09:19:10 +0200

    Ajout des @Input

--- src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Cocktail } from '../../shared/cocktail.model';
 
 @Component({
@@ -8,7 +8,7 @@ import { Cocktail } from '../../shared/cocktail.model';
 })
 export class CocktailDetailsComponent implements OnInit {
 
-  public cocktail = new Cocktail('Mojito', 'http://anotherwhiskyformisterbukowski.com/wp-content/uploads/2016/09/mojito-1.jpg', 'Le mojito, prononcé en espagnol, est un cocktail à base de rhum, de citron vert et de feuilles de menthe fraîche, né à Cuba dans les années 1910.');
+  @Input() cocktail: Cocktail;
 
   constructor() { }
 
