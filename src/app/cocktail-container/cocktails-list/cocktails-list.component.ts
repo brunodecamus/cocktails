8fa5b36624fe8cbe69ecb328bf93977e8bd1b9c1
bruno00057 <bruno.decamus@gmail.com>
2018-10-24 22:17:15 +0200

    Suppr des Input et Output. Ajout d un service

--- src/app/cocktail-container/cocktails-list/cocktails-list.component.ts
+++ src/app/cocktail-container/cocktails-list/cocktails-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Cocktail } from '../../shared/models/cocktail.model';
+import { CocktailService } from '../../shared/services/cocktail.service';
 
 @Component({
   selector: 'app-cocktails-list',
@@ -8,21 +9,23 @@ import { Cocktail } from '../../shared/models/cocktail.model';
 })
 export class CocktailsListComponent implements OnInit {
 
-  @Input() cocktails: Cocktail[];
-
-  @Output() public pick: EventEmitter<number> = new EventEmitter<number>();
-
+  public cocktails: Cocktail[];
   public activeCocktail: number = 0;
 
-  constructor() { }
+  constructor(private cocktailService: CocktailService) { }
 
   ngOnInit() {
+    this.cocktailService.cocktails.subscribe(
+      (cocktails: Cocktail[]) => {
+        this.cocktails = cocktails;
+      }
+    );
   }
 
   pickCocktail(index: number): void {
     console.log("index selected : " + index);
     this.activeCocktail = index;
-    this.pick.emit(index);
+    this.cocktailService.selectCocktail(index);
   }
 
 }
