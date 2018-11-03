bbfc46b65cf73795a9baa86738bca49eff642c5b
bruno00057 <bruno.decamus@gmail.com>
2018-11-03 09:27:04 +0100

    Initialisation formulaire new/edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CocktailsListComponent } from './cocktail-container/cocktails-list/cocktails-list.component';
 import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
 import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
-import { ActiveDirective } from './shared/directives/active.directive';
 import { PanierComponent } from './panier/panier/panier.component';
 import { IngredientListComponent } from './panier/ingredient-list/ingredient-list.component';
+import { CocktailEditComponent } from './cocktail-container/cocktail-edit/cocktail-edit.component';
+
 import { AppRouting } from './app.module-routing';
 import { PanierService } from './shared/services/panier.service';
-import { CocktailEditComponent } from './cocktail-container/cocktail-edit/cocktail-edit.component';
+import { ActiveDirective } from './shared/directives/active.directive';
 
 
 @NgModule({
@@ -29,7 +30,9 @@ import { CocktailEditComponent } from './cocktail-container/cocktail-edit/cockta
   ],
   imports: [
     BrowserModule,
-    AppRouting
+    AppRouting,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [PanierService],
   bootstrap: [AppComponent]
--- src/app/cocktail-container/cocktail-edit/cocktail-edit.component.ts
+++ src/app/cocktail-container/cocktail-edit/cocktail-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cocktail-edit',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CocktailEditComponent implements OnInit {
 
-  constructor() { }
+  public cocktailForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.cocktailForm = this.fb.group({
+      name: ['', Validators.required],
+      img: ['', Validators.required],
+      desc: ''
+    })
+  }
+
+  createCocktail() {
+    // Se charge de faire une requête POST à l’API pour sauvegarder le cocktail en DB
+    console.log("CocktailEditComponent.createCocktail : " + this.cocktailForm);
+    console.log(this.cocktailForm)
   }
 
 }
