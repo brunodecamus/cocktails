3dfaed29d2859dedf2423b41df55e50e8f04df3d
bruno00057 <bruno.decamus@gmail.com>
2018-12-30 11:22:38 +0100

    Ajout du 3ieme graph avec 3s

--- src/app/graphs/sample3/sample3.component.ts
+++ src/app/graphs/sample3/sample3.component.ts
@@ -27,6 +27,7 @@ export class Sample3Component implements OnInit {
             //yAxisID: 'gg',
             showLine: true, // disable for a single dataset
             fill: true,
+            pointRadius: 1,
             data: [
               { x: 14, y: 13 },
               { x: 10, y: 20 },
@@ -35,8 +36,9 @@ export class Sample3Component implements OnInit {
               { x: 14, y: 13 }
             ],
             // backgroundColor: '#ff6384',
-            borderWidth: 5,
-            borderColor: '#ff6384'
+            borderWidth: 2,
+            backgroundColor: 'rgba(0, 200, 0, 0.1)',
+            borderColor: 'rgb(0, 200, 0)'
           }
 
           ,
@@ -44,7 +46,8 @@ export class Sample3Component implements OnInit {
           {
             label: 'Polygone 2',
             showLine: true, // disable for a single dataset
-            fill: false,
+            fill: true,
+            pointRadius: [5, 2, 2, 2, 5],
             data: [
               { x: 2, y: 8 },
               { x: 2, y: 20 },
@@ -53,9 +56,12 @@ export class Sample3Component implements OnInit {
               { x: 2, y: 8 }
             ],
 
-            borderWidth: 1,
-            borderColor: '#006384'
+            borderWidth: 2,
+            backgroundColor: 'rgba(100, 0, 100, 0.1)',
+            borderColor: 'rgb(100, 0, 100)'
           }
+
+          
 
         ]
       },
@@ -66,6 +72,10 @@ export class Sample3Component implements OnInit {
           }
         },
         responsive: true,
+        maintainAspectRatio: true,
+        animation: {
+          duration: 800
+      },
         scales: {
           xAxes: [{
             type: 'linear',
@@ -75,35 +85,46 @@ export class Sample3Component implements OnInit {
       }
     });
 
+
+    console.log(this.myChart.data.datasets);
+    setTimeout(() => {
+      console.log("3000 ms");
+      
+      this.myChart.data.datasets.push(
+
+        {
+          label: 'Polygone 3',
+          showLine: true, // disable for a single dataset
+          fill: true,
+          pointRadius: 3,
+          data: [
+            { x: 1, y: 5 },
+            { x: 29, y: 6 },
+            { x: 31, y: 31 },
+            { x: 7, y: 9 },
+            { x: 1, y: 5 }
+          ],
+          // backgroundColor: '#ff6384',
+          borderWidth: 3,
+          backgroundColor: 'rgba(200, 200, 0, 0.1)',
+          borderColor: 'rgb(200, 200, 0)'
+        }
+
+      );
+
+      console.log(this.myChart.data.datasets);
+      // this.myChart.clear(); // OK
+      this.myChart.update();
+      this.myChart.resize();
+      
+    }, 3000);
+
+
+
+    
+
+
   }
 
-  /**
-  var scatterChart = new Chart(ctx, {
-      type: 'scatter',
-      data: {
-          datasets: [{
-              label: 'Scatter Dataset',
-              data: [{
-                  x: -10,
-                  y: 0
-              }, {
-                  x: 0,
-                  y: 10
-              }, {
-                  x: 10,
-                  y: 5
-              }]
-          }]
-      },
-      options: {
-          scales: {
-              xAxes: [{
-                  type: 'linear',
-                  position: 'bottom'
-              }]
-          }
-      }
-  });
-   *  */
 
 }
